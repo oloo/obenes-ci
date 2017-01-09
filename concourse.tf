3f7f76d8c7f8f67499f7e2c0aa9aa62462c0882a
James Onyango <jamesony@thoughtworks.com>
2017-01-08 22:40:15 -0800

    add elb for concourse

--- concourse.tf
+++ concourse.tf
@@ -1,5 +1,16 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "default_availability_zone" {
+  default = "us-east-1d"
+}
+
+variable "vpc" {
+  default = "vpc-79e3bd1d"
+}
+
+variable "subnet" {
+  default = "subnet-25f72d7d"
+}
 
 provider "aws" {
   access_key = "${var.access_key}"
@@ -7,11 +18,33 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "obene-concourse-ci" {
+resource "aws_instance" "obene-concourse-ci-instance" {
   ami = "ami-b63769a1"
   instance_type = "t2.small"
+  availability_zone = "${var.default_availability_zone}"
   tags {
-    name = "Obene Concourse CI"
-    group = "Obene"
+    Name = "Obene Concourse CI"
+    Environment = "production"
+    Group = "Obene"
+  }
+}
+
+resource "aws_elb" "obene-concourse-elb" {
+  name = "obene-concourse-elb"
+  internal = false
+  availability_zones = ["${var.default_availability_zone}"]
+  instances = ["${aws_instance.obene-concourse-ci-instance.id}"]
+
+  listener {
+    instance_port = 8080
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  tags {
+    Name = "Obene Concourse CI ELB"
+    Environment = "production"
+    Group = "Obene"
   }
 }
